55ab33cfc7288d7987e8076bfe68991d734b393b
MRNCallero <mariano.callero@cencosud.com>
2022-09-12 16:06:15 -0300

    commit story 3

--- server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const express = require('express');
-const usersRoutes = require('./api/routes/userRoutes')
-const app = express();
-app.use(express.json());
-app.get('/',(req,res)=>res.status(200).json({"msg":"Bienvenidos!"}));
-app.use(usersRoutes);
-app.listen(30000, ()=>console.log('>>>> server running... >>>>'))
-
