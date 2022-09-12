1c4b8b5960526310a525685c995de2ce167a9c3c
MarianoHernandezEscobal <mariano.escobal@cencosud.com>
2022-09-12 15:58:52 -0300

    Actualizar controlador Pictures

--- api/middleware/middlewareIDinBody.js
+++ api/middleware/middlewareIDinBody.js
@@ -1,9 +1,21 @@
 
 function middlewareIDinBody (req,res,next){
 
-    if(){
-        
+    let validacion = false;
+    if(req.query.producto && !req.params.id){
+        validacion = true;
+        req.body.idProducto = req.query.producto; 
     }
-
-    
+    if(req.params.id && !req.query.productos) { 
+        validacion = true;
+        req.body.idProducto = req.params.id;       
+    }
+    if(validacion){
+        next();
+    }else{
+        res.status(404).json({
+            msj: "Not Found"
+        });
+    }
 }
+module.exports = middlewareIDinBody;
