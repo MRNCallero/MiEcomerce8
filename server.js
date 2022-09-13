dd7ac4e060a1828adee26202a3e4dd1674c62984
SantiagoAmorin <samorinmolina@gmail.com>
2022-09-13 10:59:44 -0300

    Middlewares server y mas

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require('express');
 require('dotenv').config();
 const userRoutes = require('./api/routes/userRoutes');
-const productsRoutes = '';
-const cartRoutes = '';
+const productsRoutes = require('./api/routes/productRoutes');
+const cartRoutes = require('./api/routes/cartRoutes');
+const picturesRoutes = require('./api/routes/pictureRoutes')
 const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
-const cors = require('cors');
+//const YAML = require('yamljs');
+//const swaggerDocument = YAML.load('./swagger.yaml');
+//const cors = require('cors');
 
 //PUERTO
 const PORT = 8080;
 
 const app = express();
 
-app.use('/api/v1',userRoutes, (req,res) => {`<h1>Server funcionando en el puerto ${PORT} </h1>`});
-app.use('/api/v1',productsRoutes);
-app.use('/api/v1',cartRoutes);
+app.get('/api/v1', (req,res) => {res.send(`<h1>Server funcionando en el puerto ${PORT} </h1>`)})
+app.use('/api/v1/users',userRoutes);
+app.use('/api/v1/products',productsRoutes);
+app.use('/api/v1/carts',cartRoutes);
+app.use('/api/v1/pictures',pictureRoutes);
+
 
 
 app.listen(PORT,()=>{
