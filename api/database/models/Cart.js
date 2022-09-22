3b20e8bf3bb788afd0754f496005056035db5913
andrescorchs <andrescorchs@gmail.com>
2022-09-22 16:11:27 -0300

    update git ignore and adding model of Cart

---
+++ api/database/models/Cart.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+    const alias = "Cart";
+    const cols = {
+        id_user: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        id_product: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        }
+    }
+    const config = {
+        timestamps: true,
+        createdAt: "date"
+    };
+    const Cart = sequelize.define(alias, cols, config);
+    Cart.associate = (models) => {
+        Cart.belongsTo(models.User,{
+            as: "cartuser",
+            foreignKey: "id_user",
+        });
+        Cart.associate = (models) => {
+            Cart.belongsTo(models.Product,{
+                as: "cartproduct",
+                foreignKey: "id_product",
+            })
+        }
+    }
+    return Cart;
+}
