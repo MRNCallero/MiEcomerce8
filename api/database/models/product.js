0067349210cfef3658039dffc048980b693648e0
nicotexi <nicolastexidor344@gmail.com>
2022-09-22 15:56:55 -0300

    Agregado primera version del modelo de Product

---
+++ api/database/models/product.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Product'; 
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        title: {
+            type: dataTypes.VARCHAR(255),
+            allowNull: false
+        },
+        price: {
+            type: dataTypes.INTEGER,
+            allowNull: false
+        },
+        mostwanted: {
+            type: dataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        stock: {
+            type: dataTypes.INTEGER,
+            defaultValue: 0
+        },
+        description: {
+            type: dataTypes.VARCHAR(255)
+        },
+        id_category: dataTypes.INTEGER
+    };
+    let config = {
+        timestamps: false,
+        deletedAt: false
+    }
+    const Product = sequelize.define(alias,cols,config);
+
+
+    return Product;
+};
