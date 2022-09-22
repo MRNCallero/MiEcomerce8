c00a71ba7636829f26926c2eac105bb3569a5f71
andrescorchs <andrescorchs@gmail.com>
2022-09-22 15:35:53 -0300

    changing folder structure and configuration from env

--- api/database/config/config.js
+++ api/database/config/config.js
@@ -1,9 +1,12 @@
-{
+require('dotenv').config();
+
+const config = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER || "root",
+    "password": process.env.DB_PASS || null,
+    "database": process.env.DB_NAME || "database_development",
+    "host": process.env.DB_HOST || "127.0.0.1",
+    "port": process.env.DB_PORT || "3306",
     "dialect": "mysql"
   },
   "test": {
