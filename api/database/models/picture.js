0d5b9f50b13af1241615d67a344b7100eed74937
MarianoHernandezEscobal <mariano.escobal@cencosud.com>
2022-09-22 15:58:53 -0300

    Creacion models pictures

---
+++ api/database/models/picture.js
@@ -0,0 +1,36 @@
+
+
+
+    module.exports = (sequelize, dataTypes) =>{
+
+        const alias = "Picture";
+        const col ={
+            id:{
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            url:{
+                type: DataTypes.STRING
+            },
+            description:{
+                type: DataTypes.STRING,
+                allowNull: true
+            }
+
+        }
+        const extra = {
+            timestamps: false,
+            tableName: "Picture",
+        }
+
+        const Picture = sequelize.define(alias,col,extra);
+        Picture.associate = (models) => {
+            Picture.belongsTo(models.Product,{
+                as: "FotoProducto",
+                foreignKey: "id_product",
+            })
+        }
+
+        return Picture;
+    }
