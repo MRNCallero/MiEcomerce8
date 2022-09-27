ca63b283d96ba484769973f22b46d1d74de8f507
andrescorchs <andrescorchs@gmail.com>
2022-09-27 11:40:35 -0300

    added update functionality for a cart

--- api/controllers/cartController.js
+++ api/controllers/cartController.js
@@ -2,6 +2,7 @@
 const db = require('../database/models/index');
 const { sequelize } = require('../database/models');
 const { where } = require('sequelize');
+const e = require('cors');
 const Op = db.Sequelize.Op
 
 
@@ -13,41 +14,19 @@ const listCart = async (req, res) => {
                 id_user: id
             }
         });
-        // console.log(uCart);
-        console.log(uCart);
         if(uCart.length){
             let retList = [];
-
-            uCart.forEach(async (elem, index) =>{
+            // let index = 0
+            for(let index in uCart ){
+                console.log(uCart[index]);
+                let elem = uCart[index]
                 let prod = await db.Product.findByPk(elem.id_product);
                 retList[index] = {
                     product: prod,
                     date: elem.date,
                     quantity: elem.quantity
                 };
-            });
-            
-            // let foundProd = [];
-            // let listaAux = [];
-            // cart.cart.forEach(elem =>{
-            //         foundProd.push(productsJSON.find(el => {
-            //             return el.id == elem.product
-            //         }));})
-            // listaAux = prodListViewer(foundProd);
-            // listaAux = listaAux.map((ele,i)=>{
-            //     return {
-            //         producto:ele,
-            //         quantity:cart.cart[i].quantity
-            //     }
-            // })
-            // if(foundProd == undefined){
-            //     res.status(404).json({
-            //         ok: false,
-            //         msg: 'Producto no encontrado'
-            //     })
-            // }else{
-            //     res.status(200).json(listaAux);
-            // }
+            }
             res.status(200).json(retList);
 
         }else{
@@ -65,37 +44,87 @@ const listCart = async (req, res) => {
     }
 }
 
-const updateCart = (req, res) => {
+
+const updateStock = async (id, dec) => {
+    if(dec === 0){
+        return 0;
+    }
+    let prod = await db.Product.findByPk(id);
+    if (!prod){
+        return 0;
+    } else if (prod.stock - dec < 0){
+        await db.Product.increment({stock: (prod.stock*(-1))}, { where: {id : id} });
+        return prod.stock;
+    } else {
+        await db.Product.increment({stock: (dec*(-1))}, { where: {id : id} });
+        return dec;
+    }
+
+}
+
+const updateCart = async (req, res) => {
     const id = Number(req.params.id);
     try{
-        const carts = JSON.parse(fs.readFileSync(DB_PATH, 'utf-8'));
-        let exist = false;
-        let index = 0;
-        let updatedCart = carts.map((elem,i) => {
-            if(elem.id === id){
-                exist = true;
-                index = i;
-                elem.cart = req.body;
+        const uCart = await db.Cart.findAll({
+            where: {
+                id_user: id
             }
-            return elem;
         });
-        if(exist){
-            fs.writeFileSync(DB_PATH, JSON.stringify(updatedCart),'utf-8');
-            res.status(200).json(updatedCart[index].cart);
-        }else{
-            res.status(404).json({
-                ok: false,
-                message: "Cart does not exist"
-            });
+        let exist = [];
+        const newCart = req.body;
+        let diff = 0;
+        let quant = 0;
+        const today = new Date(Date.now());
+        for (let elem in newCart){
+            // si el elemento ya estaba en el carrito
+            exist = uCart.filter((item) => item.id_product === newCart[elem].product);
+            if (exist.length !== 0){
+                diff = await updateStock(newCart[elem].product, newCart[elem].quantity - exist[0].quantity);
+                if (diff !== 0){
+                    await db.Cart.update({ quantity: exist[0].quantity + diff, date: today.toISOString() }, {
+                        where: {
+                            [Op.and]: [{id_product: newCart[elem].product}, {id_user: id}]
+                        }
+                    });
+                }
+            } else {
+                // updateStock
+                quant = await updateStock(newCart[elem].product, newCart[elem].quantity);
+                // si hay que agregar el elemento en el carrito
+                await db.Cart.create({
+                    id_user: id,
+                    id_product: newCart[elem].product,
+                    date: today.toISOString(),
+                    quantity: quant
+                });
+            }
         }
+
+        // si hay que borrar el elemento del carrito
+        for (let del in uCart){
+            exist = newCart.filter((item) => item.product === uCart[del].id_product);
+            if(exist.length === 0){
+                await updateStock(uCart[del].id_product, ((uCart[del].quantity)*(-1)));
+                await db.Cart.destroy({
+                    where: {
+                        [Op.and]: [{id_product: uCart[del].id_product}, {id_user: id}]
+                    }
+                });
+            }
+        }
+
+        res.status(201).json({
+            ok: true,
+            message: "Cart Updated"
+        });
+
     }catch(error){
         console.log(error);
         res.status(500).json({
             ok: false,
-            message: "Error handling file"
+            message: "Error connecting to DB"
         });
     }
-
 }
 
 
--- api/routes/cartRoutes.js
+++ api/routes/cartRoutes.js
@@ -6,11 +6,10 @@ const verifyToken = require('../middleware/verifyToken');
 
 const controller = require('./../controllers/cartController');
 
-// router.use(verifyToken);
+router.use(verifyToken);
 
-// router.get('/:id', habilitarVis, controller.listCart);
-router.get('/:id', controller.listCart);
-
+router.get('/:id', habilitarVis, controller.listCart);
 router.put('/:id', habilitarMod,controller.updateCart);
+
 
 module.exports = router;
